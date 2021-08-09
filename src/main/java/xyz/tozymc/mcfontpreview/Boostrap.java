43ad456eb7cec06abdb5838fb313f0463bc6e943
Nguyễn Thanh Tân <tozymc@gmail.com>
2021-08-09 10:35:51 +0700

    Make default UI with has no function and action

--- src/main/java/xyz/tozymc/mcfontpreview/Boostrap.java
+++ src/main/java/xyz/tozymc/mcfontpreview/Boostrap.java
@@ -1,14 +1,42 @@
 package xyz.tozymc.mcfontpreview;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.HeadlessException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import javax.swing.BorderFactory;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.GroupLayout.Group;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.border.EmptyBorder;
+import xyz.tozymc.mcfontpreview.layout.SingleComponentAspectRatioKeeperLayout;
 
 public class Boostrap extends JFrame {
   private static final String TITLE = "Minecraft Font Preview";
-  private static final int MIN_WIDTH = 640;
-  private static final int MIN_HEIGHT = 360;
+  // FIXME: Need to change min size
+  private static final int MIN_WIDTH = 100;
+  private static final int MIN_HEIGHT = 100;
+  // TODO: Add non-selectable title
+  private static final String NON_SELECTABLE_ITEM = "Select your font image...";
+
+  private static final Dimension PREVIEW_PANEL_MINIMUM_SIZE = new Dimension(270, 270);
   private static final Dimension MINIMUM_SIZE = new Dimension(MIN_WIDTH, MIN_HEIGHT);
+  private static final Dimension COMBO_BOX_SIZE = new Dimension(200, 25);
+  private static final Dimension BUTTON_SIZE = new Dimension(150, 25);
+
+  private JPanel previewPanel;
+  private JComboBox<String> fontFilesComboBox;
+  private JFileChooser resourcePackChooser;
 
   Boostrap() throws HeadlessException {
     this.frameInit();
@@ -28,6 +56,71 @@ public class Boostrap extends JFrame {
     this.setVisible(true);
   }
 
+  private void initialFontFilesComboBox() {
+    fontFilesComboBox = new JComboBox<>();
+    fontFilesComboBox.setMaximumSize(COMBO_BOX_SIZE);
+    fontFilesComboBox.setPreferredSize(COMBO_BOX_SIZE);
+
+  }
+
+  private void initialPreviewPanel() {
+    previewPanel = new JPanel();
+    previewPanel.setBorder(BorderFactory.createLineBorder(Color.GRAY));
+    previewPanel.setMinimumSize(PREVIEW_PANEL_MINIMUM_SIZE);
+  }
+
+  private void initialResourcePackChooser() {
+    resourcePackChooser = new JFileChooser();
+    resourcePackChooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+  }
+
   private void initialComponents() {
+    initialFontFilesComboBox();
+    initialPreviewPanel();
+    initialResourcePackChooser();
+
+    JLabel label = new JLabel(TITLE);
+    label.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 28));
+    label.setBorder(new EmptyBorder(10, 20, 10, 20));
+
+    JButton button = new JButton("Choose resource pack...");
+    button.addActionListener(new ResourcePackChoiceListener());
+    button.setMaximumSize(BUTTON_SIZE);
+    button.setPreferredSize(BUTTON_SIZE);
+
+    GroupLayout layout = new GroupLayout(getContentPane());
+    layout.setAutoCreateGaps(true);
+    layout.setAutoCreateContainerGaps(true);
+
+    JPanel wrapperPanel = new JPanel(new SingleComponentAspectRatioKeeperLayout());
+    wrapperPanel.add(previewPanel);
+
+    Group hGroup = layout.createSequentialGroup()
+        .addGroup(layout.createParallelGroup(Alignment.CENTER)
+            .addComponent(label)
+            .addComponent(button)
+            .addComponent(fontFilesComboBox)
+            .addComponent(wrapperPanel));
+    layout.setHorizontalGroup(hGroup);
+
+    Group vGroup = layout.createSequentialGroup()
+        .addComponent(label)
+        .addComponent(button)
+        .addComponent(fontFilesComboBox)
+        .addPreferredGap(ComponentPlacement.RELATED, 10, 20)
+        .addComponent(wrapperPanel);
+    layout.setVerticalGroup(vGroup);
+
+    getContentPane().setLayout(layout);
+  }
+
+  private class ResourcePackChoiceListener implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+      if (resourcePackChooser.showOpenDialog(Boostrap.this) != JFileChooser.APPROVE_OPTION) {
+        return;
+      }
+      File file = resourcePackChooser.getSelectedFile();
+    }
   }
 }
