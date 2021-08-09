b8c7c2ae43b8425e0a46793e762920a67e14bcc4
Nguyễn Thanh Tân <tozymc@gmail.com>
2021-08-09 12:18:39 +0700

    Set lock and feel to system

--- src/main/java/xyz/tozymc/mcfontpreview/Main.java
+++ src/main/java/xyz/tozymc/mcfontpreview/Main.java
@@ -1,9 +1,22 @@
 package xyz.tozymc.mcfontpreview;
 
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 public class Main {
   public static void main(String[] args) {
-    SwingUtilities.invokeLater(Boostrap::new);
+    SwingUtilities.invokeLater(() -> {
+      setSystemLookAndFeel();
+      new Boostrap();
+    });
+  }
+
+  private static void setSystemLookAndFeel() {
+    try {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+    } catch (ReflectiveOperationException | UnsupportedLookAndFeelException e) {
+      e.printStackTrace();
+    }
   }
 }
